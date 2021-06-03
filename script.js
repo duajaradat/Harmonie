dc5ecc0c3a99f612caab5295f1c221a7c1f0195c
duajaradat <>
2021-06-03 12:41:51 +0000

    added a JS file

---
+++ script.js
@@ -0,0 +1,14 @@
+var username="sally" ;
+
+alert("Welcome to Our Beautiful World "+  username )
+
+document.write("<h3> You can Contact US to purchase items by E-mail </p>")
+
+ var answer = prompt("Do you like OneLine Art");
+
+ if(answer == "yes") {
+   alert("please contact us to get a special gift")
+   } else{
+   alert("You will change your mind after see our masterpieces")  
+   }
+   console.log(answer)
