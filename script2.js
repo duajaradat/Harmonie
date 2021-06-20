b9365355a6334bee1fa70a798939c17426a4dcaa
duajaradat <duajaradat164@gmail.com>
2021-06-20 17:11:40 +0300

    JS2 file

---
+++ script2.js
@@ -0,0 +1,18 @@
+
+let userName= window.prompt("Please,Enter your Name:");
+console.log('Your Name is '+userName);
+alert("Welcome "+userName);
+let answer1 = prompt("Do you like OneLine Art?")
+console.log(answer1)
+if (answer1== "yes"){
+    alert("That is AWESOME :)")
+}else{ alert("I promise you will enjoy ")
+}
+let answer2 = prompt("Do you want to learn how to paint? ")
+console.log(answer2)
+if (answer2 == "yes"){
+    alert("Yay, I will share painting videos in our Beautiful Website")
+}else{ alert("That's too BAD :( ")
+}
+let answer3 = prompt("Please Pick one to enter our Beautiful World: colorful pic , black&white pic")
+console.log("Your Awnser is "+answer3)
